d7ce51e85884874d1e82d28f34292d4dcbaef765
Pavol Rusnak <stick@gk2.sk>
2017-01-09 16:38:22 +0100

    remove obsolete pipe transport

--- tests/device_tests/config.py
+++ tests/device_tests/config.py
@@ -21,17 +21,8 @@ from __future__ import print_function
 import sys
 sys.path = ['../../'] + sys.path
 
-from trezorlib.transport_pipe import PipeTransport
 from trezorlib.transport_hid import HidTransport
 from trezorlib.transport_udp import UdpTransport
-
-def pipe_exists(path):
-    import os
-    try:
-        os.stat(path)
-        return True
-    except:
-        return False
 
 devices = HidTransport.enumerate()
 
@@ -43,15 +34,6 @@ if len(devices) > 0:
     DEBUG_TRANSPORT = HidTransport
     DEBUG_TRANSPORT_ARGS = (devices[0],)
     DEBUG_TRANSPORT_KWARGS = {'debug_link': True}
-
-elif pipe_exists('/tmp/pipe.trezor.to'):
-    print('Using Emulator (v1=pipe)')
-    TRANSPORT = PipeTransport
-    TRANSPORT_ARGS = ('/tmp/pipe.trezor', False)
-    TRANSPORT_KWARGS = {}
-    DEBUG_TRANSPORT = PipeTransport
-    DEBUG_TRANSPORT_ARGS = ('/tmp/pipe.trezor_debug', False)
-    DEBUG_TRANSPORT_KWARGS = {}
 
 elif True:
     print('Using Emulator (v2=udp)')
